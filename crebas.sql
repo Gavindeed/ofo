f6ae508eea5823901c96466d78fe1cabab02618d
Gavin Gong <Gavin Gong>
2017-05-21 21:57:10 +0800

    create

--- crebas.sql
+++ crebas.sql
@@ -1,19 +1,38 @@
 /*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2017/5/20 22:50:40                           */
+/* DBMS name:      MySQL 4.0                                    */
+/* Created on:     2017/5/21 21:56:29                           */
 /*==============================================================*/
 
-use ofo;
+
+drop index EndingLocation_FK on Account;
+
+drop index StartingLocation_FK on Account;
+
+drop index "User-Account_FK" on Account;
+
+drop index "Bike-Account_FK" on Account;
 
 drop table if exists Account;
+
+drop index Own_FK on Bike;
+
+drop index Locate_FK on Bike;
 
 drop table if exists Bike;
 
 drop table if exists Location;
 
+drop index "User-Recharge_FK" on Recharge;
+
 drop table if exists Recharge;
 
+drop index "Location-Repair_FK" on Repair;
+
+drop index "Bike-Repair_FK" on Repair;
+
 drop table if exists Repair;
+
+drop index Serve_FK on User;
 
 drop table if exists User;
 
@@ -22,16 +41,49 @@ drop table if exists User;
 /*==============================================================*/
 create table Account
 (
-   Account_ID           int not null,
-   Location_ID          int,
-   Bike_ID              int,
-   User_ID              int,
-   Loc_Location_ID      int,
-   Use_date             date not null,
-   Use_time             time not null,
-   Charge               float not null,
-   Left_balance         float not null,
+   Account_ID                     int                            not null,
+   Location_ID                    int,
+   Bike_ID                        int,
+   User_ID                        int,
+   Loc_Location_ID                int,
+   Use_date                       date                           not null,
+   Use_time                       time                           not null,
+   Charge                         float                          not null,
+   Left_balance                   float                          not null,
    primary key (Account_ID)
+)
+type = InnoDB;
+
+/*==============================================================*/
+/* Index: "Bike-Account_FK"                                     */
+/*==============================================================*/
+create index "Bike-Account_FK" on Account
+(
+   Bike_ID
+);
+
+/*==============================================================*/
+/* Index: "User-Account_FK"                                     */
+/*==============================================================*/
+create index "User-Account_FK" on Account
+(
+   User_ID
+);
+
+/*==============================================================*/
+/* Index: StartingLocation_FK                                   */
+/*==============================================================*/
+create index StartingLocation_FK on Account
+(
+   Loc_Location_ID
+);
+
+/*==============================================================*/
+/* Index: EndingLocation_FK                                     */
+/*==============================================================*/
+create index EndingLocation_FK on Account
+(
+   Location_ID
 );
 
 /*==============================================================*/
@@ -39,14 +91,31 @@ create table Account
 /*==============================================================*/
 create table Bike
 (
-   Bike_ID              int not null,
-   Location_ID          int,
-   User_ID              int,
-   Password             varchar(32) not null,
-   Age                  int not null,
-   Is_broken            blob not null,
-   In_use               VBIN1 not null,
+   Bike_ID                        int                            not null,
+   Location_ID                    int,
+   User_ID                        int,
+   Password                       varchar(32)                    not null,
+   Age                            int                            not null,
+   Is_broken                      blob                           not null,
+   In_use                         VBIN1                          not null,
    primary key (Bike_ID)
+)
+type = InnoDB;
+
+/*==============================================================*/
+/* Index: Locate_FK                                             */
+/*==============================================================*/
+create index Locate_FK on Bike
+(
+   Location_ID
+);
+
+/*==============================================================*/
+/* Index: Own_FK                                                */
+/*==============================================================*/
+create index Own_FK on Bike
+(
+   User_ID
 );
 
 /*==============================================================*/
@@ -54,23 +123,33 @@ create table Bike
 /*==============================================================*/
 create table Location
 (
-   Location_ID          int not null,
-   Location_name        varchar(64) not null,
+   Location_ID                    int                            not null,
+   Location_name                  varchar(64)                    not null,
    primary key (Location_ID)
-);
+)
+type = InnoDB;
 
 /*==============================================================*/
 /* Table: Recharge                                              */
 /*==============================================================*/
 create table Recharge
 (
-   Recharge_ID          int not null,
-   User_ID              int,
-   Recharge_date        date not null,
-   Recharge_amount      float not null,
-   Current_balance      float not null,
-   Recharge_approach    varchar(32) not null,
+   Recharge_ID                    int                            not null,
+   User_ID                        int,
+   Recharge_date                  date                           not null,
+   Recharge_amount                float                          not null,
+   Current_balance                float                          not null,
+   Recharge_approach              varchar(32)                    not null,
    primary key (Recharge_ID)
+)
+type = InnoDB;
+
+/*==============================================================*/
+/* Index: "User-Recharge_FK"                                    */
+/*==============================================================*/
+create index "User-Recharge_FK" on Recharge
+(
+   User_ID
 );
 
 /*==============================================================*/
@@ -78,12 +157,29 @@ create table Recharge
 /*==============================================================*/
 create table Repair
 (
-   Repair_ID            int not null,
-   Bike_ID              int,
-   Location_ID          int,
-   Repair_date          date not null,
-   Repair_time          time not null,
+   Repair_ID                      int                            not null,
+   Bike_ID                        int,
+   Location_ID                    int,
+   Repair_date                    date                           not null,
+   Repair_time                    time                           not null,
    primary key (Repair_ID)
+)
+type = InnoDB;
+
+/*==============================================================*/
+/* Index: "Bike-Repair_FK"                                      */
+/*==============================================================*/
+create index "Bike-Repair_FK" on Repair
+(
+   Bike_ID
+);
+
+/*==============================================================*/
+/* Index: "Location-Repair_FK"                                  */
+/*==============================================================*/
+create index "Location-Repair_FK" on Repair
+(
+   Location_ID
 );
 
 /*==============================================================*/
@@ -91,11 +187,20 @@ create table Repair
 /*==============================================================*/
 create table User
 (
-   User_ID              int not null,
-   Bike_ID              int,
-   User_name            varchar(64) not null,
-   Balance              float not null,
+   User_ID                        int                            not null,
+   Bike_ID                        int,
+   User_name                      varchar(64)                    not null,
+   Balance                        float                          not null,
    primary key (User_ID)
+)
+type = InnoDB;
+
+/*==============================================================*/
+/* Index: Serve_FK                                              */
+/*==============================================================*/
+create index Serve_FK on User
+(
+   Bike_ID
 );
 
 alter table Account add constraint "FK_Bike-Account" foreign key (Bike_ID)
