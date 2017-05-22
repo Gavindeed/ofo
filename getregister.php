3ad5c96c00b07181c35089fe25bb88f78600f07f
Gavin Gong <Gavin Gong>
2017-05-22 23:02:31 +0800

    register

---
+++ getregister.php
@@ -0,0 +1,26 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+
+	$conn = new mysqli($servername, $username, "", "ofo");
+
+	if($conn->connect_error) {
+		die("Connection failed! " . $conn->connect_error);
+	}
+
+	$sql = "select * from User where User_name=\"" . $_POST["User_name"] . "\"";
+	$result = $conn->query($sql);
+	if($result->num_rows > 0) {
+		print "<script>alert(\"The username has been used!\");window.location.assign(\"http://47.92.92.228/ofo/index.html\");</script>\n";
+	} else {
+		$sql = "select max(User_ID) from User";
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		$newid = $row["max(User_ID)"] + 1;
+		$sql = "insert into User(User_ID, Bike_ID, User_name, Password, Balance) values(" . $newid . ", null, \"" . $_POST["User_name"] . "\", \"" . $_POST["Passowrd"] . "\", 0)";
+		$conn->query($sql);
+		print "<script>alert(\"Register succeed!\");window.location.assign(\"http://47.92.92.228/ofo/index.html\");</script>\n";
+	}
+
+	$conn->close();
+?>
---
+++ register.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ofo小黄车管理系统</title>
+		<!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+		<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
+		<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+
+		<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+		<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+		<script type="text/javascript">
+			function check() {
+				var x = document.forms["register"]["Password"];
+				var y = document.forms["register"]["RePassword"];
+				if(x != y) {
+					alert("Retype a wrong password!");
+					return false;
+				} else if(x.length < 8) {
+					alert("Password must have a length of at least 8!");
+					return false;
+				}
+				return true;
+			}
+		</script>
+
+	</head>
+	<body>
+		
+		<div class="panel panel-default" style="margin:10%">
+			<div class="panel-heading">
+				<h3 class="panel-title">Login</h3>
+			</div>
+			<div class="panel-body">
+				<form name="register" action="getregister.php" method="post">
+					<div class="input-group" style="margin:2%">
+						<span class="input-group-addon" id="basic-addon1">@</span>
+						<input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="User_name">
+					</div>
+					<div class="input-group" style="margin:2%">
+						<span class="input-group-addon" id="basic-addon1">*</span>
+						<input type="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1" name="Password">
+					</div>
+					<div class="input-group" style="margin:2%">
+						<span class="input-group-addon" id="basic-addon1">**</span>
+						<input type="password" class="form-control" placeholder="Password again" aria-describedby="basic-addon1" name="RePassword">
+					</div>
+					<button type="submit" class="btn btn-default" onsubmit="check()">Submit</button>
+				</form>
+			</div>
+		</div>
+
+	</body>
+</html>
